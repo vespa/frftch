ee862cfa5371c02ab664b585351920aa87745210
vespa <d.vespa@gmail.com>
2018-08-31 11:51:48 -0300

    styleguidist added for docs

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Button = () => <button type="button" />;
+export default Button;
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+/* using only for react styledist */
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [],
+};
