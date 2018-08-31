63722e9c53f683c266f0a70b47b82295e645410d
D. Vespa <d.vespa@gmail.com>
2018-08-31 12:48:33 -0300

    Suport to SVG added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Logo from './assets/logo.svg';
 
 const Header = ({ children }) => {
   return (
     <header>
-      <div> logo </div>
+      <div> <Logo /> </div>
       <div> {children} </div>
     </header>
   );
