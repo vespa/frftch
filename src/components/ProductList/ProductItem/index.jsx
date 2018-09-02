0710f4817cecf51f429ed26fc068606d39aa7cf7
D. Vespa <d.vespa@gmail.com>
2018-09-02 20:24:23 -0300

    show discount behavior added

--- src/components/ProductList/ProductItem/index.jsx
+++ src/components/ProductList/ProductItem/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from './style.scss';
 
 class ProductItem extends React.Component {
   constructor(args) {
@@ -14,20 +15,53 @@ class ProductItem extends React.Component {
     return images;
   }
 
+  checkDiscountPrice() {
+    const {
+      price,
+      priceWithoutDiscount,
+      formattedPriceWithoutDiscount,
+      formattedPrice,
+    } = this.props;
+    if (price === priceWithoutDiscount) {
+      return formattedPrice;
+    }
+    let percent = priceWithoutDiscount;
+    percent = priceWithoutDiscount - price;
+    percent /= priceWithoutDiscount;
+    percent *= 100;
+    return (
+      <React.Fragment>
+        <span className={style['item__content__price--full']}>
+          {`${formattedPriceWithoutDiscount}`}
+        </span> {`| ${percent.toFixed(0)}% off ` }
+        <span className={style['item__content__price--discount']}>
+          ${formattedPrice}
+        </span>
+      </React.Fragment>
+    );
+  }
+
   render() {
     const {
-      formattedPrice,
       shortDescription,
       brand,
     } = this.props;
     const { name } = brand;
     const { currentImage } = this.state;
     return (
-      <li>
-        {name}
-        {shortDescription}
-        <img src={currentImage} alt={shortDescription} />
-        {formattedPrice}
+      <li className={style.item}>
+        <span className={style.item__content}>
+          <img src={currentImage} alt={shortDescription} />
+          <span className={style.item__content__title}>
+            {name}
+          </span>
+          <span className={style.item__content__description}>
+            <span className={style.item__content__description__text}>
+              {shortDescription}
+            </span>
+          </span>
+          {this.checkDiscountPrice()}
+        </span>
       </li>
     );
   }
@@ -37,6 +71,9 @@ ProductItem.propTypes = {
   shortDescription: PropTypes.string.isRequired,
   brand: PropTypes.shape({}).isRequired,
   formattedPrice: PropTypes.string.isRequired,
+  formattedPriceWithoutDiscount: PropTypes.string.isRequired,
+  priceWithoutDiscount: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
   // images: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
